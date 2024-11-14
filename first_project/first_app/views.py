38202727e64192fa5e0e19c86db984bc538762a1
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-11-14 18:09:51 +0530

    adding the templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello rahul")
+    details = {"name" : "Rahul", "age" : 21}
+    return render(request,"first_app/index.html", context=details)
 
 def welcome(request):
-    return HttpResponse("WELCOME DJANGO")
+    # return HttpResponse("WELCOME DJANGO")
+    return render(request,"home.html")
