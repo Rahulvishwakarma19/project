d7d0dfb18315886afda8d9b07e4a5f11ff198756
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-11-20 18:39:51 +0530

    create admin and table

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Students(models.Models):
+class Students(models.Model):
     roll_no = models.IntegerField()
     name = models.CharField(max_length=128)
     age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Students
 
 # Create your views here.
 def index(request):
-    details = {"name" : "Rahul", "age" : 21}
-    return render(request,"first_app/index.html", context=details)
+    data = Students.objects.all()
+    records = {'Student_data': data}
+    return render(request,"first_app/index.html", context=records)
 
 def welcome(request):
     # return HttpResponse("WELCOME DJANGO")
