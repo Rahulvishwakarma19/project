c4e1740584eb39ba7c36a671d0814227241297e7
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-11-20 17:05:47 +0530

    models add

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Models):
+    roll_no = models.IntegerField()
+    name = models.CharField(max_length=128)
+    age = models.IntegerField()
