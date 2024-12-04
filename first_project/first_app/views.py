b08c18600ff8ba06a7d17b4b33aa291ab15a73c9
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-12-04 18:02:57 +0530

    bootstrap add

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -6,7 +6,7 @@ from first_app.models import Students
 def index(request):
     data = Students.objects.all()
     records = {'Student_data': data}
-    return render(request,"first_app/index.html", context=records)
+    return render(request,"first_app/index_bootstrap.html", context=records)
 
 def welcome(request):
     # return HttpResponse("WELCOME DJANGO")
