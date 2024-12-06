0b55992fc636cf33265ab1617ca4cba064217b8f
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-12-06 18:00:00 +0530

    add student records

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -3,5 +3,6 @@ from first_app import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('records/',views.records, name='records'),
     path('python/',views.welcome, name='welcome'),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,9 +4,13 @@ from first_app.models import Students
 
 # Create your views here.
 def index(request):
+    
+    return render(request,"first_app/index_bootstrap.html",)
+
+def records(request):
     data = Students.objects.all()
     records = {'Student_data': data}
-    return render(request,"first_app/index_bootstrap.html", context=records)
+    return render(request, "first_app/student_records.html", context=records)
 
 def welcome(request):
     # return HttpResponse("WELCOME DJANGO")
