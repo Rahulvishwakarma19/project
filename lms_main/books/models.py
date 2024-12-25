e95889e1f64634de695551ed5a9b1a0e5c2c4c36
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2024-12-25 18:10:39 +0530

    create models

--- lms_main/books/models.py
+++ lms_main/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    name = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    publisher = models.CharField(max_length=64)
+    edition = models.SmallIntegerField()
+    pages = models.SmallIntegerField()
+    
